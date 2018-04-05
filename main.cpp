afb89fa7d634c18919f6f56cb36dd2c6fc86101a
Saad Khattak <saadrustam@gmail.com>
2018-04-04 22:57:44 -0400

    SDL2 FetchContent example

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "SDL.h"
+
+int main()
+{
+  if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Quit();
+
+  return 0;
+}
